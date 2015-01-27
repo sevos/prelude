2ac400d2e79e72e83bcf6a229deb8a06a3313da7
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2015-01-27 21:02:05 +0100

    small improvements + assets

--- personal/personal-editor.el
+++ personal/personal-editor.el
@@ -6,26 +6,30 @@
 (require 'pbcopy)
 (turn-on-pbcopy)
 
-;;(setq guru-warn-only nil)
-
-
+;; enable guru mode
+(setq guru-warn-only nil)
+;; better grep
 (prelude-require-package 'ag)
-
+;; better navigation
 (prelude-require-package 'nav)
-
+;; handling textmate snippets
 (prelude-require-package 'yasnippet)
 (yas-global-mode)
-
+;; enable zoom window
 (prelude-require-package 'zoom-window)
-
+;; handle ace jumps
 (prelude-require-package 'ace-jump-buffer)
-
 (prelude-require-package 'ace-jump-mode)
+;; enable progress bar via as nyan cat ;)
+(prelude-require-package 'nyan-mode)
+(setq nyan-bar-length 15)
+(nyan-mode)
 
+;; enable mouse
 (require 'mouse)
 (xterm-mouse-mode t)
 
-
+;; enable inertial scrolling
 (require 'inertial-scroll)
 (setq inertias-global-minor-mode-map
       (inertias-define-keymap
@@ -41,32 +45,33 @@
          ("C-v"     . inertias-up)
          ("M-v"     . inertias-down)
          ) inertias-prefix-key))
+;; configurate speed and inertial
 (setq inertias-initial-velocity-wheel 10.0)
 (setq inertias-rebound-flash nil)
 (inertias-global-minor-mode 1)
 
+;; improve performance
+(setq flx-ido-threshold 1000)
 
-(require '6502-mode "6502-mode")
-(add-to-list 'auto-mode-alist '("\\.s$" . 6502-mode))
-(add-hook '6502-mode-hook
-          (lambda ()
-            (ad-disable-advice 'yank 'around 'yank-and-indent)
-            (local-set-key (kbd "RET") 'newline-and-indent)
-            (electric-indent-mode -1)))
+(add-to-list 'auto-mode-alist '("Procfile" . conf-mode))
 
+(require 'smartparens-ruby)
+
+
+;; handle better linum mode
 (unless window-system
-  (add-hook 'linum-before-numbering-hook
-            (lambda ()
-              (setq-local linum-format-fmt
-                          (let ((w (length (number-to-string
-                                            (count-lines (point-min) (point-max))))))
-                            (concat "%" (number-to-string w) "d"))))))
+  (add-hook
+   'linum-before-numbering-hook
+   (lambda ()
+     (setq-local linum-format-fmt
+                 (let ((w (length (number-to-string
+                                   (count-lines (point-min) (point-max))))))
+                   (concat "%" (number-to-string w) "d"))))))
 
 (defun linum-format-func (line)
   (concat
    (propertize (format linum-format-fmt line) 'face 'linum)
    (propertize " " 'face 'mode-line-buffer-id)))
-
 (unless window-system
   (setq linum-format 'linum-format-func))
 (global-linum-mode)
